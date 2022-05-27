343dd0934bd00b7febda8a4d4b496938911aec5d
SamaaSaamy <sa10soma@gmail.com>
2022-05-28 00:43:17 +0200

    requirments

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,16 +5,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const verifyAuthToken = (req: Request, res: Response, next: NextFunction) => {
+const verifyAuthToken = (req: express.Request, res: express.Response, next: NextFunction) => {
     try {
-        // const authorizationHeader = req.headers.authorization as string 
-        /////////// authorization gives me an error (property 'authorization' does not exist on type 'headers') i can't fix it
-        // const token = authorizationHeader.split(' ')[1]
-        // const decoded = jwt.verify(token, String(process.env.TOKEN_SECRET))
+        var authorizationHeader = req.headers.authorization as string
+        const token = authorizationHeader.split(' ')[1]
+        const decoded = jwt.verify(token, String(process.env.TOKEN_SECRET))
 
         next()
-    } catch (error: any) { throw new Error(error.toString()) 
-}
+    } catch (error: any) {
+        throw new Error(error.toString())
+    }
 }
 
 
